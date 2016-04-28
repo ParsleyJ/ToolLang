f31feac7913e15edb64aeefbde6ccd5c95459022
ParsleyJ <parsleyjoe@gmail.com>
2016-04-28 21:56:53 +0200

    added logical operator semantics (not short-circuit)

--- src/com/parsleyj/tool/ToolBoolean.java
+++ src/com/parsleyj/tool/ToolBoolean.java
@@ -1,5 +1,11 @@
 package com.parsleyj.tool;
 
+import com.parsleyj.tool.exceptions.ToolArithmeticException;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Created by Giuseppe on 04/04/16.
  * TODO: javadoc
@@ -13,6 +19,22 @@ public class ToolBoolean extends ToolObject{
 
     public boolean getBoolValue(){
         return x;
+    }
+
+
+    public static List<ToolMethod> getNativeClassMethods() {
+        return Collections.emptyList();
+    }
+
+    public static List<ToolMethod> getNativeInstanceMethods() {
+        return Arrays.asList(
+                new PUONativeInstanceMethod<ToolBoolean>("logicalNot", BaseTypes.C_BOOLEAN,
+                        (a, mem) -> new ToolBoolean(!a.getBoolValue())),
+                new CBONativeInstanceMethod<ToolBoolean>("logicalAnd", BaseTypes.C_BOOLEAN, "x", //TODO: short-circuit
+                        (a, b, mem)-> new ToolBoolean(a.getBoolValue() && b.getBoolValue())),
+                new CBONativeInstanceMethod<ToolBoolean>("logicalOr", BaseTypes.C_BOOLEAN, "x", //TODO: short-circuit
+                        (a, b, mem)-> new ToolBoolean(a.getBoolValue() || b.getBoolValue()))
+        );
     }
 
     @Override
