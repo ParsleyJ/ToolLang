b81e44fd3d54f7e1f9663c8eb35a9b96f44401f1
ParsleyJ <parsleyjoe@gmail.com>
2016-05-29 10:06:40 +0200

    added first (incorrect) draft of extensions

--- src/com/parsleyj/tool/semantics/nametabled/DefinitionExtension.java
+++ src/com/parsleyj/tool/semantics/nametabled/DefinitionExtension.java
@@ -1,9 +1,16 @@
 package com.parsleyj.tool.semantics.nametabled;
 
+import com.parsleyj.tool.exceptions.InvalidTypeException;
 import com.parsleyj.tool.exceptions.ToolNativeException;
 import com.parsleyj.tool.memory.Memory;
+import com.parsleyj.tool.memory.Reference;
+import com.parsleyj.tool.objects.ToolClass;
+import com.parsleyj.tool.objects.ToolField;
 import com.parsleyj.tool.objects.ToolObject;
+import com.parsleyj.tool.objects.method.ToolMethod;
 import com.parsleyj.tool.semantics.base.RValue;
+
+import java.util.HashMap;
 
 /**
  * Created by Giuseppe on 27/05/16.
@@ -21,6 +28,23 @@ public class DefinitionExtension implements RValue{
 
     @Override
     public ToolObject evaluate(Memory memory) throws ToolNativeException {
+        ToolObject klassObject = klassExpression.evaluate(memory);
+        if(!klassObject.getBelongingClass().isOrExtends(memory.baseTypes().C_CLASS))
+            throw new InvalidTypeException(memory, "'"+klassExpression+"' is not a valid extensible class.");
+        ToolClass oldklass = (ToolClass) klassObject;
+        ToolClass newklass = new ToolClass(memory, oldklass.getClassName(),
+                oldklass);
+        memory.pushClassDefinitionScope(newklass);
+        ToolObject bodyResult = body.evaluate(memory);//todo: result could be the default value
+        for(Reference r: memory.getTopScope().getReferenceTable().values()){
+            newklass.addInstanceField(new ToolField(r.getReferenceType(), r.getIdentifierString(), memory.getObjectById(r.getPointedId())));
+        }
+        for(ToolMethod m: memory.getTopScope().getMethods().getAll()){
+            newklass.addInstanceMethod(m);
+        }
+        newklass.setNameTable(new HashMap<>(memory.getTopScope().getNameTable()));
+        memory.popScopeAndGC();
+        memory.loadClass(newklass);
         return null;
     }
 }
